5460888a1fe8b4780e185db304d5625d64aacf9c
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-17 11:42:12 +0700

    add queries on users table

--- internal/database/postgres/users.sql
+++ internal/database/postgres/users.sql
@@ -1,13 +1,45 @@
 -- name: ListUsers :many
 -- get all users ordered by username
-Select * 
+select * 
 from 
 gowebapp.users
 order by user_name;
 
 -- name: GetUser :one
 -- get users of particular user_id
-Select *
+select *
 from
 gowebapp.users
 where user_id = $1;
+
+-- name: GetUserWorkout :many
+-- get a particular information, exercise sets and workouts
+select u.user_id, w.workout_id, w.start_date, s.set_id, s.weight
+from
+gowebapp.users as u,
+gowebapp.workouts as w,
+gowebapp.sets as s
+where u.user_id = w.user_id
+and w.set_id = s.set_id
+and u.user_id = $1;
+
+-- name: GetUserImage :one
+-- get a particular user image
+select u.name, u.user_id, i.image_data
+from
+gowebapp.users as u,
+gowebapp.images as i
+where u.user_id = i.user_id
+and u.user_id = $1;
+
+-- name: DeleteUsers :exec
+-- delete a particular user
+delete
+from gowebapp.users
+where user_id = $1;
+
+-- name: DeleteUserImage :exec
+-- delete a particular user's image
+delete
+from gowebapp.images as i
+where i.user_id = $1;
